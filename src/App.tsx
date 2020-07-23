4188e12b8fa3e9452ba9f7fc07c9fee766db6065
Linden Holt <ace.holt@gmail.com>
2020-07-23 08:07:04 -0500

    'init'

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react'
+import { Component} from "react";
+import "./App.css";
+
+class App extends Component{
+  render(){
+    return(
+      <div className="App">
+        <h1> Hello, World! </h1>
+      </div>
+    );
+  }
+}
+
+export default App;
